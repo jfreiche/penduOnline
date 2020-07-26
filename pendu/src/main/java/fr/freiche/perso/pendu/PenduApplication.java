7e66e1dcf9951dbbfd54414baca67b219e59bad9
jfreiche <jul.freiche@gmail.com>
2020-07-26 13:50:36 +0200

    Initialisation du projet avec Spring Boot

---
+++ pendu/src/main/java/fr/freiche/perso/pendu/PenduApplication.java
@@ -0,0 +1,13 @@
+package fr.freiche.perso.pendu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenduApplication.class, args);
+	}
+
+}
---
+++ pendu/src/test/java/fr/freiche/perso/pendu/PenduApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.freiche.perso.pendu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
