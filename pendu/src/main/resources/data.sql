a934b67e696e2966214a38950e1418a48ec2b99b
jfreiche <jul.freiche@gmail.com>
2020-07-27 21:43:57 +0200

    Initialisation base de données + page d'accueil html

---
+++ pendu/src/main/resources/data.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS dictionnaire;
+	 
+CREATE TABLE dictionnaire (
+	  mot VARCHAR(100) PRIMARY KEY
+);
+
+
+INSERT INTO dictionnaire (mot) VALUES
+  ('programme'),
+  ('lithosphere'),
+  ('roquefort'),
+  ('zygomatique'),
+  ('volcan'),
+  ('pantoufle'),
+  ('test');
